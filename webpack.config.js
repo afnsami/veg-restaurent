369abe66c3c8e33ae18a12b9378828d5dc812eb0
afnsami <70536934+afnsami@users.noreply.github.com>
2026-07-13 22:27:27 +0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+
+import path from "node:path";
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  
+};
